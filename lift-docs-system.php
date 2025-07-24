1db7db70356f580bddaa46be9abc729517372ef3
MADELab Dev <105243474+MadeLabDev@users.noreply.github.com>
2025-07-24 15:31:25 -0500

    ádsad

--- lift-docs-system.php
+++ lift-docs-system.php
@@ -90,7 +90,6 @@ class LIFT_Docs_System {
         require_once LIFT_DOCS_PLUGIN_DIR . 'includes/class-lift-docs-frontend-login.php';
         require_once LIFT_DOCS_PLUGIN_DIR . 'includes/class-lift-forms.php';
         require_once LIFT_DOCS_PLUGIN_DIR . 'lib/emergency-json-fixer.php';
-        require_once LIFT_DOCS_PLUGIN_DIR . 'lib/php8-compatibility-fix.php';
     }
     
     /**
